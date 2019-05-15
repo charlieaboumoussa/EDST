7ef211bf210002f318a375dc799788af9726bcc3
charlieaboumoussa <charlieaboumoussa@outlook.com>
2019-05-15 11:06:45 +0300

    Minor fix

--- app/src/main/java/com/cbm/edst/ui/fragments/InscriptionFragment.java
+++ app/src/main/java/com/cbm/edst/ui/fragments/InscriptionFragment.java
@@ -18,6 +18,8 @@ import com.cbm.edst.common.views.SelectionEditText;
 import com.cbm.edst.common.views.SelectionModel;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InscriptionFragment extends Fragment {
 
@@ -63,6 +65,25 @@ public class InscriptionFragment extends Fragment {
         category.setSelectionModel(mCategoryModel);
         specialization.setSelectionModel(mSpecializationModel);
         courseName.setSelectionModel(mCourseNameModel);
+    }
 
+    public boolean isEmailValid(String email) {
+        String regExpn =
+                "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
+                        + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                        + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                        + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
+
+        CharSequence inputStr = email;
+
+        Pattern pattern = Pattern.compile(regExpn, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputStr);
+
+        if (matcher.matches())
+            return true;
+        else
+            return false;
     }
 }
--- app/src/main/java/com/cbm/edst/ui/fragments/registrationfragments/RegistrationFragment.java
+++ app/src/main/java/com/cbm/edst/ui/fragments/registrationfragments/RegistrationFragment.java
@@ -19,6 +19,8 @@ import com.cbm.edst.common.views.SelectionModel;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegistrationFragment extends Fragment {
 
@@ -73,5 +75,25 @@ public class RegistrationFragment extends Fragment {
             return true;
         }
         return false;
+    }
+
+    public boolean isEmailValid(String email) {
+        String regExpn =
+                "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
+                        + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                        + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                        + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
+
+        CharSequence inputStr = email;
+
+        Pattern pattern = Pattern.compile(regExpn, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputStr);
+
+        if (matcher.matches())
+            return true;
+        else
+            return false;
     }
 }
